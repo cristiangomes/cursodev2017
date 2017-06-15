d3866d2799f4fc96272734cd861755038c15c63e
Cristian Gomes <cristian@consuti.com.br>
2017-06-15 00:53:51 -0300

    criado projeto do VisualOS e projeto modelo CRUD

---
+++ VisualOS/VisualOS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VisualOS.MvcApplication" Language="C#" %>
---
+++ VisualOS/VisualOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualOS.Startup))]
+namespace VisualOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
