6a62e6793cd022a9b5abcee4882dfd898c3c673b
mateus martins <mateus_a_a_m@hotmail.com>
2017-07-04 10:52:30 -0300

    Tela de Pesquisa e Cadastro

--- VisualOS/VisualOS/Controllers/PesquisasController.cs
+++ VisualOS/VisualOS/Controllers/PesquisasController.cs
@@ -18,5 +18,26 @@ namespace VisualOS.Controllers
             ListaOS = ListaOS.Where(os => os.Data_abertura != null);
             return View(ListaOS);
         }
+
+        public ActionResult PesquisarServico()
+        {
+            Random random = new Random();
+            return View(CriaServicosMock(random.Next(123123)));
+        }
+
+        private List<tb_servico> CriaServicosMock(int qtdItens)
+        {
+            Random random = new Random();
+            List<tb_servico> listaServicos = new List<tb_servico>();
+            for (int i = 0; i < qtdItens; i++)
+            {
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                tb_servico servico = new tb_servico();
+                listaServicos.Add(servico);
+                servico.Nome = random.Next().ToString();
+                servico.Descricao = random.Next().ToString();
+            }
+            return listaServicos;
+        }
     }
 }
