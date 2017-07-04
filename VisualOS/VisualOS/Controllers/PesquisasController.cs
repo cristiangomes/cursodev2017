7ea8e56ac7ff404714ee921e3d727eb6ca05585f
Alessandro Piassi <Alessandro Piassi@NOTEBOOK>
2017-07-04 11:01:44 -0300

    Tela de pesquisa de usuario.

--- VisualOS/VisualOS/Controllers/PesquisasController.cs
+++ VisualOS/VisualOS/Controllers/PesquisasController.cs
@@ -22,11 +22,32 @@ namespace VisualOS.Controllers
 
         public ActionResult PesquisarUsuario()
         {
-            var usuarios = from u in bdusuario 
-                           select u;
-            return View("PesquisarUsuario");
+            //var usuarios = from u in db.tb_usuario 
+            //               select u;
+            return View("PesquisarUsuario",CriaListaUsuariosMOCK(10));
         }
 
+        private List<tb_usuario> CriaListaUsuariosMOCK(int qtdUsuarios)
+        {
+            List<tb_usuario> listaUsuarios = new List<tb_usuario>();
+
+            for (int i = 0; i < qtdUsuarios; i++)
+            {
+                tb_usuario usuario = new tb_usuario();
+                usuario.CPF = new Random().Next(111111111, 999999999);
+                usuario.DataNascimento = DateTime.Now;
+                usuario.DDD = new Random().Next(999);
+                usuario.Email = new Random().Next(999999999).ToString();
+                usuario.ID = new Random().Next(999999999);
+                usuario.NomeCompleto = new Random().Next(999999999).ToString();
+                usuario.Senha = new Random().Next(65837689).ToString();
+                usuario.Telefone = new Random().Next(999999999);
+                listaUsuarios.Add(usuario);
+            }
+            
+            return listaUsuarios;
+
+        }
 
 
 
