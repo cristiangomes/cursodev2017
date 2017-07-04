28c43b10356e034674285af2b622721c4e15ac2c
Mateus Gomes <Mateus Gomes@MATEUSGOMES-PC>
2017-07-04 10:49:49 -0300

    Tela e action da tela Pesquisa OS com mock

--- VisualOS/VisualOS/Controllers/PesquisasController.cs
+++ VisualOS/VisualOS/Controllers/PesquisasController.cs
@@ -14,9 +14,43 @@ namespace VisualOS.Controllers
 
         public ActionResult PesquisarOS()
         {
-            var ListaOS = from o in db.tb_ordemservico select o;
-            ListaOS = ListaOS.Where(os => os.Data_abertura != null);
+            //var ListaOS = from o in db.tb_ordemservico select o;
+            //ListaOS = ListaOS.Where(os => os.Data_abertura != null);
+            List<tb_ordemservico> ListaOS = new List<tb_ordemservico>();
+            Random random = new Random();
+            ListaOS = CriaListaOSMock(random.Next(1, 20));
             return View(ListaOS);
         }
+
+        private List<tb_ordemservico> CriaListaOSMock(int qtdItens)
+        {
+            List <tb_ordemservico> ListaOS = new List<tb_ordemservico>();
+
+            for (int i=0; i<qtdItens; i++)
+            {
+                tb_usuario usuario = new tb_usuario();
+                tb_ordemservico OS = new tb_ordemservico();
+                Random random = new Random();
+
+                usuario.CPF = random.Next(111111111, 999999999);
+                usuario.DataNascimento = DateTime.Now;
+                usuario.DDD = random.Next(01, 99);
+                usuario.Email = random.Next().ToString();
+                usuario.ID = random.Next(01, 1000);
+                usuario.NomeCompleto = random.Next().ToString();
+                usuario.Senha = random.Next().ToString();
+                usuario.Telefone = random.Next(11111111, 999999999);
+
+                OS.ID = random.Next(1, 999999999);
+                OS.tb_usuario = usuario;
+                OS.ID_Usuario_Responsavel = random.Next(1, 999999999);
+                OS.Tipo_OS = random.Next(31231231).ToString();
+                OS.Data_abertura = DateTime.Now;
+
+                ListaOS.Add(OS);
+            }
+            return ListaOS;
+        }
     }
+
 }
