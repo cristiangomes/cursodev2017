947293f6c3c72813da0c3513c994b4318d367cb8
Mateus Gomes <Mateus Gomes@MATEUSGOMES-PC>
2017-07-13 10:52:02 -0300

    Teste unitario da função MockOS

--- VisualOS/UnitTestProject/UnitTest1.cs
+++ VisualOS/UnitTestProject/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VisualOS.Controllers;
+using System.Collections.Generic;
+using VisualOS.DAL;
+using System.Web.Mvc;
 
 namespace UnitTestProject
 {
@@ -12,9 +15,15 @@ namespace UnitTestProject
         {
         }
 
+        [TestMethod]
         public void TestMockOS()
         {
+            List<tb_ordemservico> OS = new List<tb_ordemservico>();
+            VisualOS.Controllers.ControllerBase controller = new VisualOS.Controllers.ControllerBase();
+            OS = controller.CriaListaOSMock(3);
 
+            Assert.AreNotEqual(OS[0].ID, 22);
+            Assert.AreEqual(OS.Count, 3);
         }
     }
 }
